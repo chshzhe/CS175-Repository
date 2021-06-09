d78d0e83bfbdd592d48d23f522c8ca29e3a7f3a6
Shuangzhe Chen <54205081+chshzhe@users.noreply.github.com>
2021-06-10 00:24:58 +0800

    Delete CS175_final_project-master directory

--- CS175_final_project-master/FinalProject/app/src/main/java/com/example/finalproject/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.example.finalproject;
-
-public class Constants {
-    public static final String BASE_URL = "https://api-sjtu-camp-2021.bytedance.com/homework/invoke/";
-    public static final String STUDENT_ID = "xxxx";
-    public static final String USER_NAME = "ZhangSan";
-}
